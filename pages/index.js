40cf3478eb16fb6490611ef15aadc0a5ed592b0b
Victor Petit <victor.petit.35@gmail.com>
2020-07-17 17:16:15 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Home = () => (
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Learn <a href="https://nextjs.org">Next.js!</a>
       </h1>
 
       <p className="description">
