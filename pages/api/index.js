f6e1c7ba67cdaea3f8c501813f06b72f75260155
Victor PETIT <victor.petit@orange.com>
2020-07-20 09:38:49 +0200

    1.2

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
-  const { body } = req
-  res.end(`Hello ${body.name}, you just parsed the request body!`)
+module.exports = (req, res) => {
+  const { name = 'World' } = req.query
+  res.status(200).send(`Hello ${name}!`)
 }
