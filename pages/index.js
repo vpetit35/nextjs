624b169d6442bb843a6e34a9f38a1fd81e5d2a30
Victor PETIT <victor.petit@orange.com>
2020-07-20 09:29:23 +0200

    1.1

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Head from 'next/head'
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App v1.0</title>
+      <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
       <h1 className="title">
         Learn <a href="https://nextjs.org">Next.js!</a>
-		<h2>v1.0</h2>
+		<h2>v1.1</h2>
       </h1>
 
       <p className="description">
