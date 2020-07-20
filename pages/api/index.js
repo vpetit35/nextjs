96d4f7cf096762f83afc85cb53dc88a955b40671
Victor PETIT <victor.petit@orange.com>
2020-07-20 10:29:00 +0200

    fix

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,4 +1,17 @@
+const request = require('request');
 module.exports = (req, res) => {
+  request.post(response_url, {
+	  json: {
+		  text: 'ok'
+		}
+	}, (error, res, body) => {
+  if (error) {
+    console.error(error)
+    return
+  }
+  console.log(`statusCode: ${res.statusCode}`)
+  console.log(body)
+})
   res.json({
     body: req.body,
     query: req.query,
